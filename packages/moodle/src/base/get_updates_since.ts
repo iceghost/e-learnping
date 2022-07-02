eff38f854a958aaed117d1e0bba98b589bf9b2f5
Khang Nguyen Duy <khang.nguyenduycse@hcmut.edu.vn>
2022-07-02 14:44:10 +0700

    fix: truncate time value

--- packages/moodle/src/base/get_updates_since.ts
+++ packages/moodle/src/base/get_updates_since.ts
@@ -3,7 +3,7 @@ import { Update } from '..';
 export const GetUpdatesSince = {
     encode(courseid: number, since: Date) {
         return {
-            since: since.getTime() / 1000,
+            since: Math.trunc(since.getTime() / 1000),
             courseid: courseid,
             wsfunction: 'core_course_get_updates_since',
         };
